a0801e91c5fe62572d143a330bd799267abf94ff
kefbr <59842980+kefbr@users.noreply.github.com>
2022-06-09 10:15:23 -0300

    Spring Boot Starter

---
+++ src/main/java/br/com/toledofoodapi/ToledofoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.toledofoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToledofoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToledofoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/toledofoodapi/ToledofoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toledofoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToledofoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
