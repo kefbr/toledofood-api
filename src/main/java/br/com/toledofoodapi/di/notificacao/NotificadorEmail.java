d7fdbb0fe92fe8720df5e69bee7af99cecb1dff3
kefbr <59842980+kefbr@users.noreply.github.com>
2022-06-13 22:22:46 -0300

    2.26 - Acessando propriedades com @ConfigurationProperties

--- src/main/java/br/com/toledofoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/br/com/toledofoodapi/di/notificacao/NotificadorEmail.java
@@ -2,16 +2,20 @@ package br.com.toledofoodapi.di.notificacao;
 
 import br.com.toledofoodapi.di.modelo.Cliente;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Setter
 @Component
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 public class NotificadorEmail implements Notificador {
-    private String hostServidorSmtp;
+    @Autowired
+    private NotificadorProperties notificadorProperties;
     @Override
     public void notificar(Cliente cliente, String mensagem){
         System.out.printf("Notificando %s através do e-mail %s usando STMP %s: %s %n",
-                cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
+                cliente.getNome(), cliente.getEmail(),
+                notificadorProperties.getHostServidor()+":"+notificadorProperties.getPortaServidor(),
+                mensagem);
     }
 }
