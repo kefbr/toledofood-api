ee2cd21f50e276eaa980d9d75dfd7e183cbcd68f
kefbr <59842980+kefbr@users.noreply.github.com>
2022-06-14 23:05:29 -0300

    3.15. Conhecendo e usando o Lombok

--- src/main/java/br/com/toledofoodapi/domain/model/Cozinha.java
+++ src/main/java/br/com/toledofoodapi/domain/model/Cozinha.java
@@ -1,37 +1,25 @@
 package br.com.toledofoodapi.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Objects;
 
-@Setter
-@Getter
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String nome;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return id != null && Objects.equals(id, cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
