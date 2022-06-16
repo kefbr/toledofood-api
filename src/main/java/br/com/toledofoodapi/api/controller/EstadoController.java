0e3a4968d3b24431ad35658483eb472d356794ae
kefbr <59842980+kefbr@users.noreply.github.com>
2022-06-16 00:47:35 -0300

    4.14. Consultando Singleton Resource com GET e @PathVariable

--- src/main/java/br/com/toledofoodapi/api/controller/EstadoController.java
+++ src/main/java/br/com/toledofoodapi/api/controller/EstadoController.java
@@ -3,7 +3,6 @@ package br.com.toledofoodapi.api.controller;
 import br.com.toledofoodapi.domain.model.Estado;
 import br.com.toledofoodapi.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +16,8 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public List<Estado> listar1() {
-        System.out.println("Listar 1");
         return estadoRepository.listar();
     }
-
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public List<Estado> listar2() {
-        System.out.println("Listar 2");
-        return estadoRepository.listar();
-    }
-
 }
