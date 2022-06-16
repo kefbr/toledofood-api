ff25b239284acbc41e4070575fc2d885a0296cb6
kefbr <59842980+kefbr@users.noreply.github.com>
2022-06-15 23:55:24 -0300

    4.13. Implementando content negotiation para retornar JSON ou XML

--- src/main/java/br/com/toledofoodapi/api/controller/EstadoController.java
+++ src/main/java/br/com/toledofoodapi/api/controller/EstadoController.java
@@ -3,6 +3,7 @@ package br.com.toledofoodapi.api.controller;
 import br.com.toledofoodapi.domain.model.Estado;
 import br.com.toledofoodapi.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,15 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
-    @GetMapping
-    public List<Estado> listar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Estado> listar1() {
+        System.out.println("Listar 1");
+        return estadoRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Estado> listar2() {
+        System.out.println("Listar 2");
         return estadoRepository.listar();
     }
 
