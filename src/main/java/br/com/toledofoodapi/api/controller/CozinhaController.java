699b11cdcbce80378bada5884245088e3e632cd3
kefbr <59842980+kefbr@users.noreply.github.com>
2022-06-19 22:58:11 -0300

    4.19. Definindo o status da resposta HTTP com @ResponseStatus

--- src/main/java/br/com/toledofoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/toledofoodapi/api/controller/CozinhaController.java
@@ -4,11 +4,13 @@ import br.com.toledofoodapi.api.model.CozinhasXmlWrapper;
 import br.com.toledofoodapi.domain.model.Cozinha;
 import br.com.toledofoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,6 +34,7 @@ public class CozinhaController {
         return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cozinhaId}")
     public Cozinha buscar(@PathVariable Long cozinhaId){
         return cozinhaRepository.buscar(cozinhaId);
