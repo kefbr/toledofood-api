88459d547f65c5e338900185e39e99cad130f34b
kefbr <59842980+kefbr@users.noreply.github.com>
2022-07-01 15:06:38 -0300

    4.30. Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/br/com/toledofoodapi/api/controller/RestauranteController.java
+++ src/main/java/br/com/toledofoodapi/api/controller/RestauranteController.java
@@ -1,12 +1,18 @@
 package br.com.toledofoodapi.api.controller;
 
+import br.com.toledofoodapi.domain.excetion.EntidadeNaoEncontradaException;
 import br.com.toledofoodapi.domain.model.Restaurante;
 import br.com.toledofoodapi.domain.repository.RestauranteRepository;
+import br.com.toledofoodapi.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +23,9 @@ public class RestauranteController {
 
     @Autowired
     private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CadastroRestauranteService cadastroRestauranteService;
 
     @GetMapping
     public List<Restaurante> listar() {
@@ -34,4 +43,16 @@ public class RestauranteController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante){
+        try {
+            restaurante = cadastroRestauranteService.salvar(restaurante);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(restaurante);
+        }catch (EntidadeNaoEncontradaException e){
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
+    }
 }
--- src/main/java/br/com/toledofoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/br/com/toledofoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -28,7 +28,7 @@ public class RestauranteRepositoryImpl implements RestauranteRepository {
 
     @Transactional
     @Override
-    public Restaurante adicionar(Restaurante restaurante) {
+    public Restaurante salvar(Restaurante restaurante) {
         return manager.merge(restaurante);
     }
 
